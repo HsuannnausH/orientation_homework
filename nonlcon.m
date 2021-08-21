dc7e83280783e1245627950a4e02c35ae32e0364
HsuannnausH <jhsuanhsu@gmail.com>
2021-08-21 22:32:45 +0800

    Optimization
    Done!!!!!

--- nonlcon.m
+++ nonlcon.m
@@ -1,14 +1,22 @@
-function [ g,geq ] = nonlcon( disp_node2,stress,yeildStress )
+function [ g,geq ] = nonlcon(r)
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
     
+    % Finite Element Method
+    [disp_node2_x,disp_node2_y,stress,yieldStress] = finiteElementMethod(r(1,:),r(2,:));
+
+
     % Q(2) <= 0.02
-    % g(1) = Q(2)-0.02 <= 0
-    g(1) = disp_node2 - 0.02    
+    % g(1) = abs(Q(3))-0.02 <= 0
+    % g(2) = abs(Q(4))-0.02 <= 0
+    g(1) = sqrt(disp_node2_x.^2 + disp_node2_y.^2) - 0.02;    
+    %g(2) = abs(disp_node2_y) - 0.02;    
     
     % abs(stress) <= YieldStress = 250 MPa
-    % 
+    % g(3) = abs(stress) - yield <= 0
+    for i = 1:10
+        g(i + 1) = abs(stress(i,:)) - yieldStress;
+    end
 
     geq = [];
 end
-
--- obj.m
+++ obj.m
@@ -1,17 +1,21 @@
-function f = obj(r1,r2,length,density)
+function f = obj(r)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-    
+
+    % Variable
+    length1 = 9.14;
+    length2 = sqrt(length1.^2*2);
+    density = 7860;     % 7860 kg * m^-3
     f = 0;
     
     % Element 1~6 
     for i = 1:6
-        f = f + density * pi * r1.^2 * length;
+        f = f + density * pi * r(1,:).^2 * length1;
     end
     
     % Element 7~10
     for i = 1:4
-        f = f + density * pi * r2.^2 * length;
+        f = f + density * pi * r(2,:).^2 * length2;
     end
 end
 
